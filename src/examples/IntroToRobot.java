e0b3c431de27aaacae0c2056336567e83a3cacda
yrdavis <yrd1@hotmail.com>
2020-04-02 17:29:11 -0700

    A program to have a robot draw 4 squares

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -8,42 +8,40 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class IntroToRobot {
 	public static void main(String[] args) {
-		//count the number of squares drawn
+		// count the number of squares drawn
 		int square = 1;
-		//initialize the degree the robot is turning
+		// initialize the degree the robot is turning
 		int degree = 90;
-		
-		//instantiate a new robot
+
+		// instantiate a new robot
 		Robot r2d2 = new Robot();
-		
-		//set the robot's pen down, determine it's width and the drawing speed of the robot
+
+		// set the robot's pen down, determine it's width and the drawing speed of the
+		// robot
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
 		r2d2.setSpeed(5);
-		
-		//do...while loop counts the number of squares and changes the color of the drawing pen
-		do 	{	
-		//change the color of the drawing line		
-		r2d2.setRandomPenColor();
-		//for loop draws the 4 walls of the squares
-		for (int i = 0; i < 4; i++) {
-	
-			r2d2.turn(degree);
-			r2d2.move(100);
-		}
-		square++;
-			if (square ==2) {
+
+		// do...while loop counts the number of squares and changes the color of the
+		// drawing pen
+		do {
+			// change the color of the drawing line
+			r2d2.setRandomPenColor();
+			// for loop draws the 4 walls of the squares
+			for (int i = 0; i < 4; i++) {
+
+				r2d2.turn(degree);
+				r2d2.move(100);
+			}
+			square++;
+			if (square == 2) {
 				degree = -90;
-			}
-			else if 
-			(square ==3) {
+			} else if (square == 3) {
 				r2d2.move(-100);
-			}
-			else if (square ==4){
+			} else if (square == 4) {
 				degree = 90;
-	
+
 			}
-	}
-		while (square <= 4);
+		} while (square <= 4);
 	}
 }
